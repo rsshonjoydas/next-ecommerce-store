5e3992321540b323fdeb89602997956c545bbe40
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-09-13 22:05:04 +0600

    cart page

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -5,9 +5,11 @@ import { useEffect, useState } from 'react';
 
 import Button from '@/components/ui/button';
 import useCart from '@/hooks/use-cart';
+import { useRouter } from 'next/navigation';
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const router = useRouter();
 
   const cart = useCart();
 
@@ -21,7 +23,10 @@ const NavbarActions = () => {
 
   return (
     <div className='ml-auto items-center gap-x-4'>
-      <Button className='flex items-center rounded-full bg-black px-4 py-2'>
+      <Button
+        onClick={() => router.push('/cart')}
+        className='flex items-center rounded-full bg-black px-4 py-2'
+      >
         <ShoppingBag size={20} color='white' />
         <span className='ml-2 text-sm font-medium text-white'>
           {cart.items.length}
