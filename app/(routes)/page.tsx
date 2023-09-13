92c1b533f6178117207008ddc90862b926b0b687
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-09-13 18:29:23 +0600

    show billboard in home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,21 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import getBillboard from '@/actions/get-billboard';
+import Billboard from '@/components/ui/billboard';
+import Container from '@/components/ui/container';
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard(
+    `${process.env.NEXT_PUBLIC_BILLBOARD_ID}`
+  );
+
+  return (
+    <Container>
+      <div className='space-y-10 pb-10'>
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
