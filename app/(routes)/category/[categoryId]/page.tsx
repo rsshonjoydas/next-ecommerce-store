4180f489aa15974a587159fe632502fe5b0d9710
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-09-13 18:59:11 +0600

    filter product using color and size

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,10 +1,14 @@
 import getCategory from '@/actions/get-category';
+import getColors from '@/actions/get-color';
 import getProducts from '@/actions/get-products';
+import getSizes from '@/actions/get-sizes';
 
 import Billboard from '@/components/ui/billboard';
 import Container from '@/components/ui/container';
 import NoResults from '@/components/ui/no-results';
 import ProductCard from '@/components/ui/product-card';
+import Filter from './components/filter';
+import MobileFilters from './components/mobile-filters';
 
 export const revalidate = 0;
 
@@ -28,6 +32,8 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
     sizeId: searchParams.sizeId,
   });
 
+  const sizes = await getSizes();
+  const colors = await getColors();
   const category = await getCategory(params.categoryId);
 
   return (
@@ -35,6 +41,13 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
       <Container>
         <Billboard data={category.billboard} />
         <div className='px-4 sm:px-6 lg:px-8 pb-24'>
+          <div className='lg:grid lg:grid-cols-5 lg:gap-x-8'>
+            <MobileFilters sizes={sizes} colors={colors} />
+            <div className='hidden lg:block'>
+              <Filter valueKey='sizeId' name='Sizes' data={sizes} />
+              <Filter valueKey='colorId' name='Colors' data={colors} />
+            </div>
+          </div>
           <div className='mt-6 lg:col-span-4 lg:mt-0'>
             {products.length === 0 && <NoResults />}
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
