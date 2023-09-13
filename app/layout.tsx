5a2368405efa156f1588b880f13840df9f061bdb
RS Shonjoy <85416231+rsshonjoydas@users.noreply.github.com>
2023-09-13 19:14:43 +0600

    preview product model

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from '@/components/footer';
 import Navbar from '@/components/navbar';
+import ModalProvider from '@/providers/modal-provider';
 import type { Metadata } from 'next';
 import { Urbanist } from 'next/font/google';
 import './globals.css';
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={font.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -5,18 +5,27 @@ import Image from 'next/image';
 
 import Currency from '@/components/ui/currency';
 import IconButton from '@/components/ui/icon-button';
+import usePreviewModal from '@/hooks/use-preview-modal';
 import { Product } from '@/types';
 import { useRouter } from 'next/navigation';
+import { MouseEventHandler } from 'react';
 
 interface ProductCard {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const previewModal = usePreviewModal();
   const router = useRouter();
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
   };
 
   return (
@@ -34,7 +43,10 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         />
         <div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5'>
           <div className='flex gap-x-6 justify-center'>
-            <IconButton icon={<Expand size={20} className='text-gray-600' />} />
+            <IconButton
+              onClick={onPreview}
+              icon={<Expand size={20} className='text-gray-600' />}
+            />
             <IconButton
               icon={<ShoppingCart size={20} className='text-gray-600' />}
             />
